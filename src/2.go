a2cde138323b47054b1de302d7d12c91a3b498a8
retzlerquibodeaux658 <retzlerquibodeaux658@gmail.com>
2025-03-07 18:59:09 +0000

    Add 2.go

---
+++ src/2.go
@@ -0,0 +1,11 @@
+
+func main() {
+	// Declare a variable to store the sum of 2 and 3
+	var result int
+
+	// Add 2 and 3 and store the result in "result"
+	result = 2 + 3
+
+	// Print the value of "result"
+	fmt.Println(result)
+}
