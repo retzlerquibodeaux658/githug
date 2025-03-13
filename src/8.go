daa4dd12a598bfb3766d64008f36c4d43ae23233
retzlerquibodeaux658 <retzlerquibodeaux658@gmail.com>
2025-03-13 19:35:34 +0000

    Add 8.go

---
+++ src/8.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(rand.Intn(10)) // prints a random integer between 0 and 9
+}
